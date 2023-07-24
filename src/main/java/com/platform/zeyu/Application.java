485aaed3ee05ab1777e2d425a8a6235bf8fa8843
Thomas <jx@ThomasdeMacBook-Pro.local>
2023-07-24 22:08:15 +0800

    initial Commit

---
+++ src/main/java/com/platform/zeyu/Application.java
@@ -0,0 +1,17 @@
+package com.platform.zeyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 孙泽宇
+ * Data 2023.7.24 build new project
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/platform/zeyu/ZeyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.zeyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeyuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
